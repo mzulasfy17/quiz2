15d872bd8228fa70dbc749f7634804c82c9f7a9d
Zul <m.zulasfi17@gmail.com>
2024-06-06 20:10:11 +0700

    first commit

---
+++ includes/config.php
@@ -0,0 +1,13 @@
+<?php
+$config = array(
+    "site" => "http://localhost/akademik",
+    "server" => "D:/xampp/htdocs/akademik",
+    "driver" => "mysql",
+    "database" => "akademik",
+    "user" => "root",
+    "password" => "",
+    "host" => "localhost",
+    "port" => 3306,
+    "token" => "test"
+);
+?>
